3c6b34523549fff8cc19eaa569957f5221be48cb
mdsahidulc243223 <mdr073621@gmail.com>
2026-03-02 11:37:00 +0600

    template string & arrow function

---
+++ arrow_function.js
@@ -0,0 +1,11 @@
+// single line hole second backet use kora lage na
+const add=(n1)=>n1+5;
+console.log(add(5));
+// multiple line hole second bracket use kora lage
+const doMath=(x,y)=>{
+    const add=x+y;
+    const sub=x-y;
+    const result=add*sub;
+    return result;
+}
+console.log(doMath(8,4));
---
+++ template_string.js
@@ -0,0 +1,4 @@
+const firstNam="md"
+const lastName=" Sahidul Islam"
+const abouitMe=`My name is ${firstNam} ${lastName}`;
+console.log(abouitMe);
